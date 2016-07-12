8050e45cb57c2ab73fb7db0a3f88953bcc659850
thejohnhoffer <hoffexi@gmail.com>
2016-07-12 14:57:13 -0400

    reformatted for binding overlay

--- load.js
+++ load.js
@@ -6,7 +6,6 @@
 // Settings without input as url terms as /index.html?...&...
 //
 var g_z = 0;                                  // &z=
-var g_seg = true;                             // &no_seg
 var g_width = 1024;                           // &width=
 var g_height = 1024;                          // &height=
 var server_name = 'viper.krash.net:2001';     // &server=
@@ -16,7 +15,10 @@ var g_depth = 1;                              // &depth=
 //
 // The master viewer (the one that the user is driving)
 //
+var g_tileSize = 512;
 var g_master_viewer = null;
+var g_maxLevel = Math.min(5, Math.ceil(Math.log2(g_width/g_tileSize)));
+var g_minLevel = 0;
 
 //--------------------------------------
 //
@@ -24,6 +26,7 @@ var g_master_viewer = null;
 //
 //--------------------------------------
 function parse_args() {
+
   // After the first ? mark, Break the search terms by each joining & sign
   var args = decodeURI(document.location.search.substring(1)).split('&');
   var clean = (str) => str ? str.replace(new RegExp('\/$'),'') : true;
@@ -34,37 +37,71 @@ function parse_args() {
   },{});
 };
 
+function getTileUrl( level, x, y ){
+
+    level = g_maxLevel - level;
+    x *= g_tileSize;
+    y *= g_tileSize;
+
+    return "http://" + server_name + "/data/?datapath=" + data_path +
+           "&start=" + x + "," + y + "," + g_z + "&mip=" + level +
+           "&size=" + g_tileSize + "," + g_tileSize + ",1";
+}
+
+//-----------------------------------
+//
+// create_ol - Create an Overlay
+//
+//-----------------------------------
+function create_ol(callback) {
+
+    async = new XMLHttpRequest();
+    async.onreadystatechange = () => {
+      if (async.readyState == 4 && async.status == 200) {
+        callback(create_ts(), async.responseText);
+      }
+    };
+    async.open("GET", getTileUrl(0,0,0)+"&segmentation=y&segcolor=y", true);
+    async.send();
+}
+
 //-----------------------------------
 //
 // create_ts - Create a TileSource
 //
-//    z: the Z position of the tile
-//    seg: true to load a segmentation, false to load an image
-//
 //-----------------------------------
-function create_ts(z) {
-      var ts = {
-          height: g_height,
-          width:  g_width,
-          tileSize: 512,
-          minLevel: 0,
-          maxLevel: Math.min(5, Math.ceil(Math.log2(g_width/512))),
+function create_ts() {
 
-          getTileUrl: function( level, x, y ){
-
-              level = this.maxLevel - level;
-              x = x*this.tileSize;
-              y = y*this.tileSize;
-
-              url = "http://" + server_name +
-                     "/data/?datapath=" + data_path +
-                     "&start=" + x + "," + y + "," + z +
-                     "&mip=" + level +
-                     "&size=" + this.tileSize + "," + this.tileSize + ",1"
-              return url
-          }
+    var ts = {
+        height: g_height,
+        width:  g_width,
+        tileSize: g_tileSize,
+        minLevel: g_minLevel,
+        maxLevel: g_maxLevel,
+        getTileUrl: getTileUrl
     }
     return ts;
+}
+
+//-----------------------------------
+//
+// bind_sea = Bind Overlay to Tilesource
+//
+//-----------------------------------
+function bind_sea(ts,ol,container_id) {
+
+  g_master_viewer = OpenSeadragon({
+      id:            container_id,
+      prefixUrl:     "images/",
+      navigatorSizeRatio: 0.25,
+      minZoomImageRatio: 0.5,
+      maxZoomPixelRatio: 10,
+      showNavigationControl: true,
+      animationTime: 0,
+      imageLoaderLimit: 3,
+      tileSources:   ts,
+      timeout: 120000
+  });
 
 }
 
@@ -77,43 +114,17 @@ function create_ts(z) {
 //     seg: true to display segmentation, false to display image
 //
 //-----------------------------------
-function create_viewer(z, visible) {
+function create_viewer(ts,ol) {
 
     // create dom element
-    var container_id = null;
+    var container_id = 'viewer_' + g_z;
     var style = 'background-color:black;position:absolute;top:0px;left:0px;width:100%;height:100%';
-    var z_index = 0;
-    container_id = 'viewer_' + z;
-    z_index = 1;
-
-    if (! visible) {
-      z_index = 0;
-    }
-    style = style + ";z-index:" + z_index;
-
     $('#viewers').append('<div id="'+container_id+'" class="viewers" style="'+style+'"></div>');
 
-    var ts = create_ts(z);
-    g_master_viewer = OpenSeadragon({
-        id:            container_id,
-        prefixUrl:     "images/",
-        navigatorSizeRatio: 0.25,
-        minZoomImageRatio: 0.5,
-        maxZoomPixelRatio: 10,
-        showNavigationControl: true,
-        animationTime: 0,
-        imageLoaderLimit: 3,
-        tileSources:   ts,
-        timeout: 120000
-      });
-
-    var mv_elem = document.getElementById(g_master_viewer.id)
-    mv_elem.style['z-index'] = 1;
-    g_master_viewer.setVisible(true);
-    viewer.innerTracker.keyHandler = null;
-    viewer.innerTracker.keyDownHandler = null;
-
-    return viewer;
+    // Join tiles and overlays
+    bind_sea(ts,ol,container_id);
+    g_master_viewer.innerTracker.keyHandler = null;
+    g_master_viewer.innerTracker.keyDownHandler = null;
 };
 
 window.onload = function() {
@@ -128,8 +139,8 @@ window.onload = function() {
   g_height = get_size('height') || g_height;
   g_width = get_size('width') || g_width;
   g_depth = get_size('depth') || g_depth;
-  g_seg = args['no_seg'] ? false: true;
   g_z = get_size('z') || g_z;
 
-  create_viewer(g_z, true);
+  // Make image data
+  create_ol(create_viewer);
 }
