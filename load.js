3d3c56dd1fc545a620a2f42db39a33e877e90593
thejohnhoffer <hoffexi@gmail.com>
2016-07-12 13:50:27 -0400

    removed unnecessary functions

--- load.js
+++ load.js
@@ -17,21 +17,6 @@ var g_depth = 1;                              // &depth=
 // The master viewer (the one that the user is driving)
 //
 var g_master_viewer = null;
-var g_master_seg_viewer = null;
-//
-// The slave viewer (ones w/o user input)
-//
-var g_slave_viewers = [];
-//
-// The previous viewer (z-1)
-//
-var g_prev_viewer = null;
-var g_prev_seg_viewer = null;
-//
-// The next viewer (z+1)
-//
-var g_next_viewer = null;
-var g_next_seg_viewer = null;
 
 //--------------------------------------
 //
@@ -57,7 +42,7 @@ function parse_args() {
 //    seg: true to load a segmentation, false to load an image
 //
 //-----------------------------------
-function create_ts(z, seg) {
+function create_ts(z) {
       var ts = {
           height: g_height,
           width:  g_width,
@@ -76,58 +61,11 @@ function create_ts(z, seg) {
                      "&start=" + x + "," + y + "," + z +
                      "&mip=" + level +
                      "&size=" + this.tileSize + "," + this.tileSize + ",1"
-              if (seg) {
-                  url = url + "&segmentation=y&segcolor=y"
-              }
               return url
           }
     }
     return ts;
 
-}
-
-//----------------------------------
-//
-// set_viewer_to_master - make this viewer the master viewer
-//
-// also unsets the old master and returns it
-//----------------------------------
-function set_viewer_to_master(viewer) {
-  var old_master = g_master_viewer;
-  if (old_master) {
-    old_master.removeAllHandlers();
-    var ov_elem = document.getElementById(old_master.id);
-    ov_elem.style['z-index'] = 0
-  }
-  g_master_viewer = viewer;
-  viewer.addHandler('pan', propagate_viewport);
-  viewer.addHandler('zoom', propagate_viewport);
-  var mv_elem = document.getElementById(g_master_viewer.id)
-  mv_elem.style['z-index'] = 1;
-  g_master_viewer.setVisible(true);
-  return old_master;
-}
-
-//----------------------------------
-//
-// set_viewer_to_seg_master - make this viewer the segmentation master viewer
-//
-// also unsets the old master and returns it
-//----------------------------------
-function set_viewer_to_seg_master(viewer) {
-  var old_master = g_master_seg_viewer;
-  if (old_master) {
-    old_master.removeAllHandlers();
-    var ov_elem = document.getElementById(old_master.id);
-    ov_elem.style['z-index'] = 0
-  }
-  g_master_seg_viewer = viewer;
-  viewer.addHandler('pan', propagate_seg_viewport);
-  viewer.addHandler('zoom', propagate_seg_viewport);
-  var mv_elem = document.getElementById(g_master_seg_viewer.id)
-  mv_elem.style['z-index'] = 2;
-  g_master_seg_viewer.setVisible(true);
-  return old_master;
 }
 
 //----------------------------------
@@ -139,21 +77,16 @@ function set_viewer_to_seg_master(viewer) {
 //     seg: true to display segmentation, false to display image
 //
 //-----------------------------------
-function create_viewer(z, visible, seg) {
+function create_viewer(z, visible) {
 
 
     // create dom element
     var container_id = null;
     var style = 'background-color:black;position:absolute;top:0px;left:0px;width:100%;height:100%';
     var z_index = 0;
-    if (seg) {
-      container_id = 'seg_viewer_' + z;
-      style = style + ";opacity:0.5";
-      z_index = 2;
-    } else {
-      container_id = 'viewer_' + z;
-      z_index = 1;
-    }
+    container_id = 'viewer_' + z;
+    z_index = 1;
+
     if (! visible) {
       z_index = 0;
     }
@@ -161,8 +94,8 @@ function create_viewer(z, visible, seg) {
 
     $('#viewers').append('<div id="'+container_id+'" class="viewers" style="'+style+'"></div>');
 
-    var ts = create_ts(z, seg);
-    var viewer = OpenSeadragon({
+    var ts = create_ts(z);
+    g_master_viewer = OpenSeadragon({
         id:            container_id,
         prefixUrl:     "images/",
         navigatorSizeRatio: 0.25,
@@ -176,50 +109,14 @@ function create_viewer(z, visible, seg) {
         timeout: 120000
       });
 
-
-    is_master = visible & ! seg;
-    if (is_master) {
-      set_viewer_to_master(viewer);
-    } else if (visible) {
-      set_viewer_to_seg_master(viewer);
-    } else {
-      g_slave_viewers.push(viewer);
-    }
+    var mv_elem = document.getElementById(g_master_viewer.id)
+    mv_elem.style['z-index'] = 1;
+    g_master_viewer.setVisible(true);
     viewer.innerTracker.keyHandler = null;
     viewer.innerTracker.keyDownHandler = null;
 
-    // viewer.addHandler('animation-finish', this.store_viewpoint.bind(this));
-    // viewer.addHandler('tile-drawn', this.propagate_viewpoint.bind(this));
-
-
     return viewer;
 };
-
-
-//-------------------------------
-//
-// propagate_viewport - propagate the viewport settings from the master
-//                      to all slaves
-//
-//-------------------------------
-function propagate_viewport() {
-  g_slave_viewers.forEach(function(viewer) {
-      viewer.viewport.panTo(g_master_viewer.viewport.getCenter(), true);
-      viewer.viewport.zoomTo(g_master_viewer.viewport.getZoom(), null, true);
-  });
-
-}
-
-//-------------------------------
-//
-// propagate_seg_viewport - propagate the segmentation viewport to the master
-//
-//-------------------------------
-function propagate_seg_viewport() {
-  g_master_viewer.viewport.panTo(g_master_seg_viewer.viewport.getCenter(), true);
-  g_master_viewer.viewport.zoomTo(g_master_seg_viewer.viewport.getZoom(), null, true);
-}
-
 
 window.onload = function() {
 
@@ -236,20 +133,5 @@ window.onload = function() {
   g_seg = args['no_seg'] ? false: true;
   g_z = get_size('z') || g_z;
 
-  create_viewer(g_z, true, false);
-  if (g_seg) {
-    create_viewer(g_z, true, true);
-  }
-  if (g_z > 0) {
-      g_prev_viewer = create_viewer(g_z - 1, false, false);
-      if (g_seg) {
-        g_prev_seg_viewer = create_viewer(g_z - 1, false, true);
-      }
-  }
-  if (g_z < g_depth) {
-      g_next_viewer = create_viewer(g_z + 1, false, false);
-      if (g_seg) {
-        g_next_seg_viewer = create_viewer(g_z + 1, false, true);
-      }
-  }
+  create_viewer(g_z, true);
 }
