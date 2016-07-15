c11ecc9b2ea340031540a2622f8bebdf3bbcd841
thejohnhoffer <hoffexi@gmail.com>
2016-07-15 09:45:44 -0400

    cleanup

--- load.js
+++ load.js
@@ -22,8 +22,8 @@ way = function(e) {
       timeout :               120000
     }
   ];
-  var lay = new this.Lay(preset[0]);
-  lay.out(preset[1]);
+  var lay = new this.Lay(preset);
+  lay.over.onload = lay.bindSea.bind(lay);
 }
 //-----------------------------------
 //
@@ -33,42 +33,37 @@ way = function(e) {
 way.prototype.Lay = function(preterms) {
 
     // Change the inputs if passed as url terms
-    var terms = this.doTerms( preterms, decodeURI(document.location.search.substring(1)) );
-    Object.keys(preterms).forEach((str) => this[str] = terms[str] || preterms[str]);
-    var maxLevel = Math.ceil(Math.log2(this.width/this.tileSize))
-    this.maxLevel = Math.min(this.maxLevel, maxLevel);
+    var terms = this.doTerms( preterms[0], decodeURI(document.location.search.substring(1)) );
+    Object.keys(preterms[0]).forEach((str) => this[str] = terms[str] || preterms[0][str]);
+    this.maxLevel = Math.min(this.maxLevel, Math.ceil(Math.log2(this.width/this.tileSize)));
 
-    // create dom element
+    // Make low layer
+    var base = new Object();
     this.id = 'seer_' + preterms.z;
+    Object.assign(base,this).getTileUrl = this.getTile.bind(base);
+
+    // Make high layer
+    this.over = new Image();
+    this.over.src = this.getTile(0,0,0)+"&segmentation=y&segcolor=y";
+    this.seaTerms = Object.assign(preterms[1], {id: this.id, tileSources: base});
+
+    // put a section in the DOM
     var eye_elem = document.createElement('div');
     Object.assign(eye_elem,{className:'seers', id:this.id});
     document.body.appendChild(eye_elem);
 };
 
-way.prototype.Lay.prototype.out = function(preterms) {
+way.prototype.Lay.prototype.bindSea = function () {
 
-    var base = new Object();
-    this.over = new Image();
-    this.over.src = this.getTile(0,0,0)+"&segmentation=y&segcolor=y";
-    Object.assign(base,this).getTileUrl = this.getTile.bind(base);
-    Object.assign(preterms, {id: this.id, tileSources: base});
-
-    this.over.onload = this.bindSea(preterms);
-};
-
-way.prototype.Lay.prototype.bindSea = function (terms) {
-
-  var seer = OpenSeadragon(terms);
-  var overlay = seer.canvasOverlay({
-    onRedraw: () => {
-      overlay.context2d().globalAlpha = 0.4;
-      overlay.context2d().drawImage(this.over,0,0,this.width,this.height);
-    },
-    clearBeforeRedraw:true
-  });
-
+  var seer = OpenSeadragon(this.seaTerms);
   seer.innerTracker.keyDownHandler = null;
   seer.innerTracker.keyHandler = null;
+
+  var doRedraw = () => {
+    layer.context2d().globalAlpha = 0.4;
+    layer.context2d().drawImage(this.over,0,0,this.width,this.height);
+  }
+  var layer = seer.canvasOverlay({onRedraw: doRedraw});
 };
 
 way.prototype.Lay.prototype.getTile = function( level, x, y ) {
