feb002747550d23bc21eb9909489f6ebff710072
thejohnhoffer <hoffexi@gmail.com>
2016-08-19 10:15:07 -0400

    name change

---
+++ view.js
@@ -0,0 +1,33 @@
+var J = J || {};
+//-----------------------------------
+//
+// J.Viewer - test webGL overlay atop OpenSeaDragon
+//
+//-----------------------------------
+
+J.Viewer = function(baseLayer) {
+}
+
+J.Viewer.prototype.init = function() {
+
+    // Open a seaDragon with two layers
+    var openSD = OpenSeadragon({
+        tileSources: 'zoomable/fractal/fractal.dzi',
+        id: this.container || 'viaWebGL',
+        prefixUrl: 'lib/images/'
+    });
+
+    // Make a link to webGL
+    var seaGL = new SeaDragonGL();
+    seaGL.vShader = 'shaders/square.glsl';
+    seaGL.fShader = 'shaders/edge.glsl';
+
+    var load = function(callback, e) {
+        // via webGL
+        callback(e);
+    }
+
+    seaGL['tile-loaded'] = load;
+
+    seaGL.init(openSD);
+}
