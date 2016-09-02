708c5726439cf969896d7ae2e202385a3b3dc80d
thejohnhoffer <hoffexi@gmail.com>
2016-09-02 16:19:04 -0400

    better async

--- tools/seaDragonGL.js
+++ tools/seaDragonGL.js
@@ -17,53 +17,60 @@ SeaDragonGL = function(openSD) {
             e.rendered.drawImage(this.viaGL.toCanvas(input), 0, 0, input.width, input.height);
         }
     };
-    this['tile-loaded'] = function(callback, e) {
-        callback(e);
-    };
-    this['tile-drawing'] = function(callback, e) {
-        if (e.tile.loaded !==1) {
-            e.tile.loaded = 1;
+    this.defaults = {
+        'tile-loaded': function(callback, e) {
             callback(e);
+        },
+        'tile-drawing': function(callback, e) {
+            if (e.tile.loaded !==1) {
+                e.tile.loaded = 1;
+                callback(e);
+            }
         }
     };
     this.openSD = openSD;
+    this.viaGL = new ViaWebGL();
 };
 
 SeaDragonGL.prototype = {
     // Map to viaWebGL and openSeadragon
     init: function() {
-        // Map both objects
-        this.viaGL = new ViaWebGL();
-        this.openSD.addHandler('open',this.merge.bind(this));
+        var open = this.merger.bind(this);
+        this.openSD.addHandler('open',open);
         return this;
     },
-    // Merge with viaGL
-    merge: function(e) {
-        // Take GL height and width from OpenSeaDragon
-        this.height = this.openSD.source.getTileHeight();
-        this.width = this.openSD.source.getTileWidth();
-        // Add all ViaWebGL properties shared by this seadragonGL
-        for (var key of Object.keys(this.viaGL).filter(Object.hasOwnProperty,this)) {
-            this.viaGL[key] = this[key];
-        }
-        this.viaGL.init();
-    },
-    // Set up OpenSeadragon events
+    // User adds events
     addHandler: function(key,custom) {
+        if (key in this.defaults){
+            this[key] = this.defaults[key];
+        }
         if (typeof custom == 'function') {
             this[key] = custom;
         }
-        if (['tile-loaded','tile-drawing'].indexOf(key) >= 0) {
-            this.seadragonHandler(key);
+    },
+    // Merge with viaGL
+    merger: function(e) {
+        // Take GL height and width from OpenSeaDragon
+        this.width = this.openSD.source.getTileWidth();
+        this.height = this.openSD.source.getTileHeight();
+        for (var key of this.and(this.viaGL)) {
+            this.viaGL[key] = this[key];
+        }
+        this.viaGL.init().then(this.adder.bind(this));
+    },
+    // Add all seadragon properties
+    adder: function(e) {
+        for (var key of this.and(this.defaults)) {
+            var handler = this[key].bind(this);
+            var interface = this.interface[key].bind(this);
+            this.openSD.addHandler(key, function(e) {
+                handler.call(this, interface, e);
+            });
         }
     },
-    // Set up OpenSeadragon events
-    seadragonHandler: function(key) {
-        var handler = this[key].bind(this);
-        var interface = this.interface[key].bind(this);
-        this.openSD.addHandler(key, function(e) {
-            handler.call(this, interface, e);
-        });
+    // Joint keys
+    and: function(obj) {
+      return Object.keys(obj).filter(Object.hasOwnProperty,this);
     },
     // Add your own button to OSD controls
     button: function(terms) {
