924230247930f812c434673710d2bf344d20d179
thejohnhoffer <hoffexi@gmail.com>
2016-09-10 00:41:29 -0400

    rect vertex

--- demo/shaders/fragment/rect.glsl
+++ demo/shaders/fragment/rect.glsl
@@ -1,12 +1,7 @@
 precision mediump float;
-uniform vec2 u_tile_where;
-uniform vec2 u_tile_shape;
-uniform sampler2D u_tile;
 varying vec2 v_tile_pos;
 
 void main() {
 
-  vec2 where = u_tile_where + u_tile_shape*v_tile_pos;
-
-  gl_FragColor = vec4(0., where.y, where.x, 1.);
+  gl_FragColor = vec4(0., v_tile_pos.y, v_tile_pos.x, 1.);
 }
