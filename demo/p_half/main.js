d087684862cd32cedea0a8712c26a6be935ed4b5
thejohnhoffer <hoffexi@gmail.com>
2016-09-12 12:27:04 -0400

    whole and half

---
+++ demo/p_half/main.js
@@ -0,0 +1,25 @@
+
+window.onload = function(e){
+
+    // Make the two layers
+    var sourcer = function(x) {
+        return {
+            tileSource: '../../images/babel/babel.dzi',
+            x: x
+        }
+    }
+
+    // Open a seadragon with two layers
+    var openSD = OpenSeadragon({
+        maxZoomPixelRatio: 10,
+        tileSources: [sourcer(0),sourcer(.5)],
+        prefixUrl: '../../images/icons/',
+        id: 'viaWebGL'
+    });
+
+    openSD.addHandler('tile-loaded', function(e) {
+        if (e.tiledImage.getBounds().x) {
+            e.tiledImage.setOpacity(.5);
+        }
+    });
+}
