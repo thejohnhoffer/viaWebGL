ca241df95e621f62eab5cefd6ba3a4148ce39091
John Hoffer <john@hoff.in>
2018-05-15 13:50:36 -0400

    now a node module

---
+++ tools/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+window.OpenSeadragon = require('openseadragon');
+require('./openSeadragonGL');
+require('./viaWebGL');
+
+module.exports = {
+  "ViaWebGL": ViaWebGL,
+  "openSeadragonGL": openSeadragonGL
+} 
